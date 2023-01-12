d4dbf7a7a725779387e94b980ce0573d62a768f3
정석찬 <95221819+standard-Chan@users.noreply.github.com>
2023-01-12 10:19:32 +0900

    script.js
    새로고침 시, 첫 페이지로 안가게 수정

--- script/script.js
+++ script/script.js
@@ -19,8 +19,8 @@
         window.addEventListener('load', function() {
           // body의 height를 살짝 늘리는 코드
         document.body.style.height = (document.documentElement.clientHeight + 5) + 'px';
-        // scroll를 제어 하는 코드
-        setTimeout(scrollTo, 0, 0, 1);
+        // 새로고침 시 scroll를 제어 하는 코드
+        //setTimeout(scrollTo, 0, 0, 1);
         }, false);
 
 
